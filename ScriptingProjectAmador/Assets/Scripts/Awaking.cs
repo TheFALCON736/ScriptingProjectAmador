204057a38b1ff27824b851d91b0c59745c212cf7
TheFALCON736 <angel7amador06@gmail.com>
2022-11-16 10:50:52 -0800

    Awaking
    Awaking scripts were added.

---
+++ ScriptingProjectAmador/Assets/Scripts/Awaking.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awaking : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
